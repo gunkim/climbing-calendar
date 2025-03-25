0ac5a88081a7624f9b71a9112f65b3accb7cfcd7
PARKGE <phg5459@naver.com>
2025-03-25 23:04:32 +0900

    ScheduleEntity 추가

---
+++ src/main/java/github/gunkim/climbingcalendar/infrastructure/jpa/schedule/entity/ScheduleEntity.java
@@ -0,0 +1,39 @@
+package github.gunkim.climbingcalendar.infrastructure.jpa.schedule.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity(name = "schedule")
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ScheduleEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Integer userId;
+    private Integer climbingGymId;
+    private String title;
+    private String memo;
+    private Instant scheduleDate;
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ScheduleEntity that = (ScheduleEntity) o;
+        return Objects.equals(id, that.id) && Objects.equals(userId, that.userId) && Objects.equals(climbingGymId, that.climbingGymId) && Objects.equals(title, that.title) && Objects.equals(memo, that.memo) && Objects.equals(scheduleDate, that.scheduleDate) && Objects.equals(createdAt, that.createdAt) && Objects.equals(updatedAt, that.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userId, climbingGymId, title, memo, scheduleDate, createdAt, updatedAt);
+    }
+}

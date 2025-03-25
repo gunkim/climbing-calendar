2f00c4812db5555cc49c0d698ca2c1661bbf3dff
PARKGE <phg5459@naver.com>
2025-03-25 21:26:52 +0900

    saveAll 추가

--- src/main/java/github/gunkim/climbingcalendar/domain/schedule/model/Schedule.java
+++ src/main/java/github/gunkim/climbingcalendar/domain/schedule/model/Schedule.java
@@ -1,6 +1,7 @@
 package github.gunkim.climbingcalendar.domain.schedule.model;
 
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.experimental.Accessors;
 
@@ -8,7 +9,6 @@ import java.time.Instant;
 
 @Getter
 @Accessors(fluent = true)
-@AllArgsConstructor
 public class Schedule {
     private final Long id;
     private Long userId;
@@ -19,17 +19,29 @@ public class Schedule {
     private Instant createdAt;
     private Instant updatedAt;
 
+    @Builder(access = AccessLevel.PRIVATE)
+    public Schedule(Long id, Long userId, Long climbingGymId, String title, String memo, Instant scheduleDate, Instant createdAt, Instant updatedAt) {
+        this.id = id;
+        this.userId = userId;
+        this.climbingGymId = climbingGymId;
+        this.title = title;
+        this.memo = memo;
+        this.scheduleDate = scheduleDate;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
     public static Schedule create(Long userId, Long climbingGymId, String title, String memo, Instant scheduleDate) {
         Instant now = Instant.now();
-        return new Schedule(
-                null,
-                userId,
-                climbingGymId,
-                title,
-                memo,
-                scheduleDate,
-                now,
-                now
-        );
+
+        return Schedule.builder()
+                .userId(userId)
+                .climbingGymId(climbingGymId)
+                .title(title)
+                .memo(memo)
+                .scheduleDate(scheduleDate)
+                .createdAt(now)
+                .updatedAt(now)
+                .build();
     }
 }

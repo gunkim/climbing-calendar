a01fd9c75a5915314f915c76fd621d77b70925e2
PARKGE <phg5459@naver.com>
2025-03-28 02:17:54 +0900

    GET /api/v1/climbing-gyms/:id/levels 매핑

--- src/main/java/github/gunkim/climbingcalendar/api/climbinggym/ClimbingGymController.java
+++ src/main/java/github/gunkim/climbingcalendar/api/climbinggym/ClimbingGymController.java
@@ -1,9 +1,12 @@
 package github.gunkim.climbingcalendar.api.climbinggym;
 
+import github.gunkim.climbingcalendar.api.climbinggym.response.GetClimbingGymOfLevelResponse;
 import github.gunkim.climbingcalendar.api.climbinggym.response.GetClimbingGymResponse;
 import github.gunkim.climbingcalendar.domain.climbinggym.service.GetClimbingGymService;
+import github.gunkim.climbingcalendar.domain.climbinggym.service.GetLevelService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,11 +17,19 @@ import java.util.List;
 @RequestMapping("/api/v1/climbing-gyms")
 public class ClimbingGymController {
     private final GetClimbingGymService getClimbingGymService;
+    private final GetLevelService getLevelService;
 
     @GetMapping
     public List<GetClimbingGymResponse> getClimbingGyms() {
         return getClimbingGymService.getClimbingGyms().stream()
                 .map(GetClimbingGymResponse::from)
+                .toList();
+    }
+
+    @GetMapping("/{id}/levels")
+    public List<GetClimbingGymOfLevelResponse> getClimbingGymOfLevels(@PathVariable("id") Long id) {
+        return getLevelService.getLevels(id).stream()
+                .map(GetClimbingGymOfLevelResponse::from)
                 .toList();
     }
 }

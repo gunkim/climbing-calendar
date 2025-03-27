af442be4448746efab3c9ffeb4c3cc355334656a
PARKGE <phg5459@naver.com>
2025-03-28 02:15:46 +0900

    findByClimbingGymId 메소드 추가

--- src/main/java/github/gunkim/climbingcalendar/infrastructure/jpa/climbinggym/dao/LevelDao.java
+++ src/main/java/github/gunkim/climbingcalendar/infrastructure/jpa/climbinggym/dao/LevelDao.java
@@ -3,5 +3,9 @@ package github.gunkim.climbingcalendar.infrastructure.jpa.climbinggym.dao;
 import github.gunkim.climbingcalendar.infrastructure.jpa.climbinggym.entity.LevelEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface LevelDao extends JpaRepository<LevelEntity, Long> {
+    Optional<List<LevelEntity>> findByClimbingGymId(Long climbingGymId);
 }

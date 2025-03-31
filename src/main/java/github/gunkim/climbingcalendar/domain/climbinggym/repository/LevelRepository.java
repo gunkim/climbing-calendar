39f9a1cdbdcff3bfc86c08f6b1e375b8f5f27618
PARKGE <phg5459@naver.com>
2025-03-31 23:22:14 +0900

    findById 메소드 추가

--- src/main/java/github/gunkim/climbingcalendar/domain/climbinggym/repository/LevelRepository.java
+++ src/main/java/github/gunkim/climbingcalendar/domain/climbinggym/repository/LevelRepository.java
@@ -3,7 +3,10 @@ package github.gunkim.climbingcalendar.domain.climbinggym.repository;
 import github.gunkim.climbingcalendar.domain.climbinggym.model.Level;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface LevelRepository {
     List<Level> findByClimbingGymId(Long climbingGymId);
+
+    Optional<Level> findById(Long id);
 }

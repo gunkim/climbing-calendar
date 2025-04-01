b5e174945a427b9bf6c64024d16f2e1d4f87bce1
PARKGE <phg5459@naver.com>
2025-04-01 22:09:30 +0900

    Level 및 Schedule ID 타입 변경
    기존 Long 타입을 LevelId와 ScheduleId 타입으로 변경하여 타입 안정성을 높였습니다. 이를 통해 잘못된 데이터 타입 사용으로 인한 오류를 방지할 수 있게 되었습니다.

--- src/main/java/github/gunkim/climbingcalendar/domain/schedule/model/ClearItem.java
+++ src/main/java/github/gunkim/climbingcalendar/domain/schedule/model/ClearItem.java
@@ -1,10 +1,13 @@
 package github.gunkim.climbingcalendar.domain.schedule.model;
 
+import github.gunkim.climbingcalendar.domain.climbinggym.model.id.LevelId;
+import github.gunkim.climbingcalendar.domain.schedule.model.id.ScheduleId;
+
 public record ClearItem(
-        Long levelId,
+        LevelId levelId,
         int count
 ) {
-    public Clear toClear(Long scheduleId) {
+    public Clear toClear(ScheduleId scheduleId) {
         return Clear.create(scheduleId, levelId, count);
     }
 }

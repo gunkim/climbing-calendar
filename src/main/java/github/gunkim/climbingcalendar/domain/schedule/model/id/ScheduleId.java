d74a2a8885f792c99d5ac2365aaa0fe2c2169cd7
PARKGE <phg5459@naver.com>
2025-04-01 22:07:30 +0900

    새로운 ScheduleId 값 객체 추가
    ScheduleId 값을 관리하기 위한 레코드 클래스를 생성했습니다. 이를 통해 일정 식별자를 보다 명확하고 안전하게 다룰 수 있습니다.

---
+++ src/main/java/github/gunkim/climbingcalendar/domain/schedule/model/id/ScheduleId.java
@@ -0,0 +1,9 @@
+package github.gunkim.climbingcalendar.domain.schedule.model.id;
+
+public record ScheduleId(
+        Long value
+) {
+    public static ScheduleId from(Long value) {
+        return new ScheduleId(value);
+    }
+}

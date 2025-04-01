a671b68b3a1c62f52fe5c9ab645d082378ea005f
PARKGE <phg5459@naver.com>
2025-04-01 22:15:11 +0900

    스케줄 업데이트 메서드에 VO 타입 적용
    스케줄 및 클라이밍짐 ID를 VO로 변경하여 업데이트 메서드에 적용했습니다. 이를 통해 명확한 타입으로 ID를 처리하고 관련 로직의 가독성과 안전성을 향상시켰습니다.

--- src/main/java/github/gunkim/climbingcalendar/domain/schedule/service/UpdateScheduleService.java
+++ src/main/java/github/gunkim/climbingcalendar/domain/schedule/service/UpdateScheduleService.java
@@ -1,7 +1,9 @@
 package github.gunkim.climbingcalendar.domain.schedule.service;
 
+import github.gunkim.climbingcalendar.domain.climbinggym.model.id.ClimbingGymId;
 import github.gunkim.climbingcalendar.domain.schedule.model.ClearItem;
 import github.gunkim.climbingcalendar.domain.schedule.model.Schedule;
+import github.gunkim.climbingcalendar.domain.schedule.model.id.ScheduleId;
 import github.gunkim.climbingcalendar.domain.schedule.repository.ClearRepository;
 import github.gunkim.climbingcalendar.domain.schedule.repository.ScheduleRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,19 +19,19 @@ public class UpdateScheduleService {
     private final ClearRepository clearRepository;
     private final GetScheduleService getScheduleService;
 
-    public void updateSchedule(Long id, Long climbingGymId, String title, Instant scheduleDate, String memo, List<ClearItem> clears) {
+    public void updateSchedule(ScheduleId id, ClimbingGymId climbingGymId, String title, Instant scheduleDate, String memo, List<ClearItem> clears) {
         Schedule schedule = saveSchedule(id, climbingGymId, title, scheduleDate, memo);
-        saveClears(schedule.id(), clears);
+        saveClears(id, clears);
     }
 
-    private Schedule saveSchedule(Long id, Long climbingGymId, String title, Instant scheduleDate, String memo) {
+    private Schedule saveSchedule(ScheduleId id, ClimbingGymId climbingGymId, String title, Instant scheduleDate, String memo) {
         Schedule schedule = scheduleRepository.save(getScheduleService.getSchedule(id));
         schedule.update(climbingGymId, title, memo, scheduleDate);
         return schedule;
 
     }
 
-    private void saveClears(long scheduleId, List<ClearItem> clears) {
+    private void saveClears(ScheduleId scheduleId, List<ClearItem> clears) {
         clearRepository.deleteByScheduleId(scheduleId);
         clearRepository.saveAll(clears.stream()
                 .map(clear -> clear.toClear(scheduleId))

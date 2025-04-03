98dfd2a0ce434a87d009a5eee4df1f231fd53b0a
PARKGE <phg5459@naver.com>
2025-04-04 02:49:34 +0900

    기능 추가: Level 관련 메서드 확장 및 엔티티 변환 로직 추가
    LevelRepository에 findByIdsIn 및 findAll 메서드를 추가하고, LevelEntity에서 빌더 패턴을 활용한 변환 로직(from) 메서드를 도입했습니다. Repository, DAO, Domain, Entity 전반에 걸쳐 관련 변경 사항을 반영했습니다.

--- backend/src/main/java/github/gunkim/climbingcalendar/infrastructure/jpa/climbinggym/repository/LevelRepositoryImpl.java
+++ backend/src/main/java/github/gunkim/climbingcalendar/infrastructure/jpa/climbinggym/repository/LevelRepositoryImpl.java
@@ -25,7 +25,23 @@ public class LevelRepositoryImpl implements LevelRepository {
     }
 
     @Override
+    public List<Level> findByIdsIn(List<LevelId> levelIds) {
+        return levelDao.findByIdIn(levelIds.stream()
+                        .map(LevelId::value)
+                        .toList()).stream()
+                .map(LevelEntity::toDomain)
+                .toList();
+    }
+
+    @Override
     public Optional<Level> findById(LevelId id) {
         return levelDao.findById(id.value()).map(LevelEntity::toDomain);
+    }
+
+    @Override
+    public List<Level> findAll() {
+        return levelDao.findAll().stream()
+                .map(LevelEntity::toDomain)
+                .toList();
     }
 }

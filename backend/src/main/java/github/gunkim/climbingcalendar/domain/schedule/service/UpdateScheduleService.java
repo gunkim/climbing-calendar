c40a76a27db11203a67a4e8983f5e27b662f7d78
PARKGE <phg5459@naver.com>
2025-04-05 02:15:41 +0900

    일정 업데이트 사용자 확인
    일정을 업데이트할 때 사용자 권한 검사를 추가했습니다.

--- backend/src/main/java/github/gunkim/climbingcalendar/domain/schedule/service/UpdateScheduleService.java
+++ backend/src/main/java/github/gunkim/climbingcalendar/domain/schedule/service/UpdateScheduleService.java
@@ -1,10 +1,12 @@
 package github.gunkim.climbingcalendar.domain.schedule.service;
 
 import github.gunkim.climbingcalendar.domain.climbinggym.model.id.ClimbingGymId;
+import github.gunkim.climbingcalendar.domain.schedule.exception.UnauthorizedScheduleException;
 import github.gunkim.climbingcalendar.domain.schedule.model.ClearCommand;
 import github.gunkim.climbingcalendar.domain.schedule.model.Schedule;
 import github.gunkim.climbingcalendar.domain.schedule.model.id.ScheduleId;
 import github.gunkim.climbingcalendar.domain.schedule.repository.ScheduleRepository;
+import github.gunkim.climbingcalendar.domain.user.model.id.UserId;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,12 +23,13 @@ public class UpdateScheduleService {
     private final ScheduleRepository scheduleRepository;
 
     public Schedule updateSchedule(ScheduleId scheduleId,
+                                   UserId userId,
                                    ClimbingGymId climbingGymId,
                                    String title,
                                    Instant scheduleDate,
                                    String memo,
                                    List<ClearCommand> clearCommands) {
-        Schedule updatedSchedule = updateSchedule(scheduleId, climbingGymId, title, scheduleDate, memo);
+        Schedule updatedSchedule = updateSchedule(scheduleId, userId, climbingGymId, title, scheduleDate, memo);
         scheduleRepository.update(updatedSchedule);
 
         updateClearService.updateClears(scheduleId, clearCommands);
@@ -34,11 +37,15 @@ public class UpdateScheduleService {
     }
 
     private Schedule updateSchedule(ScheduleId scheduleId,
+                                    UserId userId,
                                     ClimbingGymId climbingGymId,
                                     String title,
                                     Instant scheduleDate,
                                     String memo) {
         Schedule schedule = getScheduleService.getScheduleById(scheduleId);
+        if (!schedule.userId().equals(userId)) {
+            throw new UnauthorizedScheduleException("User is not authorized to update this schedule.");
+        }
         schedule.update(climbingGymId, title, memo, scheduleDate);
 
         return schedule;

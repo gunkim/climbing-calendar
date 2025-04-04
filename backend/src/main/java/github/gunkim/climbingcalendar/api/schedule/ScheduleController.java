4bb5483b10ac984bd3158e4d1609d138e7c0f1b3
PARKGE <phg5459@naver.com>
2025-04-05 00:25:57 +0900

    불필요한 import 코드 삭제

--- backend/src/main/java/github/gunkim/climbingcalendar/api/schedule/ScheduleController.java
+++ backend/src/main/java/github/gunkim/climbingcalendar/api/schedule/ScheduleController.java
@@ -5,11 +5,12 @@ import github.gunkim.climbingcalendar.api.schedule.model.requeset.CreateSchedule
 import github.gunkim.climbingcalendar.api.schedule.model.requeset.UpdateScheduleRequest;
 import github.gunkim.climbingcalendar.api.schedule.model.response.GetScheduleResponse;
 import github.gunkim.climbingcalendar.domain.climbinggym.model.id.ClimbingGymId;
-import github.gunkim.climbingcalendar.domain.schedule.model.ClearWithLevel;
 import github.gunkim.climbingcalendar.domain.schedule.model.ScheduleWithClear;
-import github.gunkim.climbingcalendar.domain.schedule.model.ScheduleWithClimbingGym;
 import github.gunkim.climbingcalendar.domain.schedule.model.id.ScheduleId;
-import github.gunkim.climbingcalendar.domain.schedule.service.*;
+import github.gunkim.climbingcalendar.domain.schedule.service.CreateScheduleService;
+import github.gunkim.climbingcalendar.domain.schedule.service.DeleteScheduleService;
+import github.gunkim.climbingcalendar.domain.schedule.service.ScheduleWithClearReader;
+import github.gunkim.climbingcalendar.domain.schedule.service.UpdateScheduleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -51,12 +52,14 @@ public class ScheduleController implements ScheduleResource {
 
     @Override
     public void createSchedule(AuthenticatedUser authenticatedUser, CreateScheduleRequest createScheduleRequest) {
-        createScheduleService.createSchedule(authenticatedUser.userId(), ClimbingGymId.from(createScheduleRequest.climbingGymId()), createScheduleRequest.title(), createScheduleRequest.memo(), createScheduleRequest.scheduleDate(), createScheduleRequest.clearList());
+        createScheduleService.createSchedule(authenticatedUser.userId(), ClimbingGymId.from(createScheduleRequest.climbingGymId()), createScheduleRequest.title(),
+                createScheduleRequest.memo(), createScheduleRequest.scheduleDate(), createScheduleRequest.clearList());
     }
 
     @Override
     public void updateSchedule(Long id, UpdateScheduleRequest updateScheduleRequest) {
-        updateScheduleService.updateSchedule(ScheduleId.from(id), ClimbingGymId.from(updateScheduleRequest.climbingGymId()), updateScheduleRequest.title(), updateScheduleRequest.scheduleDate(), updateScheduleRequest.memo(), updateScheduleRequest.clearList());
+        updateScheduleService.updateSchedule(ScheduleId.from(id), ClimbingGymId.from(updateScheduleRequest.climbingGymId()), updateScheduleRequest.title(),
+                updateScheduleRequest.scheduleDate(), updateScheduleRequest.memo(), updateScheduleRequest.clearList());
     }
 
     @Override

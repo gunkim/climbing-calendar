4edf4b644addadb89f7b71d94fbf08cdb9e50ddd
PARKGE <phg5459@naver.com>
2025-04-07 00:31:05 +0900

    클라이밍 체육관 조회 로직 최적화
    스케줄 조회 시 중복된 클라이밍 체육관 조회를 방지하도록 로직을 최적화했습니다.

--- backend/src/main/java/github/gunkim/climbingcalendar/domain/schedule/service/ScheduleWithClimbingGymReader.java
+++ backend/src/main/java/github/gunkim/climbingcalendar/domain/schedule/service/ScheduleWithClimbingGymReader.java
@@ -1,6 +1,7 @@
 package github.gunkim.climbingcalendar.domain.schedule.service;
 
 import github.gunkim.climbingcalendar.domain.climbinggym.model.ClimbingGym;
+import github.gunkim.climbingcalendar.domain.climbinggym.model.id.ClimbingGymId;
 import github.gunkim.climbingcalendar.domain.climbinggym.service.GetClimbingGymService;
 import github.gunkim.climbingcalendar.domain.schedule.model.Schedule;
 import github.gunkim.climbingcalendar.domain.schedule.model.ScheduleWithClimbingGym;
@@ -10,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -24,8 +27,14 @@ public class ScheduleWithClimbingGymReader {
     }
 
     public List<ScheduleWithClimbingGym> getSchedules(UserId userId) {
-        return getScheduleService.getSchedulesByUserId(userId).stream()
-                .map(schedule -> new ScheduleWithClimbingGym(schedule, getClimbingGymService.getClimbingGym(schedule.climbingGymId())))
+        List<Schedule> schedules = getScheduleService.getSchedulesByUserId(userId);
+        Map<ClimbingGymId, ClimbingGym> climbingGymMap = schedules.stream()
+                .map(Schedule::climbingGymId)
+                .distinct()
+                .collect(Collectors.toMap(climbingGymId -> climbingGymId, getClimbingGymService::getClimbingGym));
+
+        return schedules.stream()
+                .map(schedule -> new ScheduleWithClimbingGym(schedule, climbingGymMap.get(schedule.climbingGymId())))
                 .toList();
     }
 }

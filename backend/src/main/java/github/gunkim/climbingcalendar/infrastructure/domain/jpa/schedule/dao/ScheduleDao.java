78262117c11806453ca46e655ff417ecb9a588e0
gunkim <gunkim.dev@gmail.com>
2025-04-07 22:27:16 +0900

    @Param 애노테이션 추가로 쿼리 파라미터 명시

--- backend/src/main/java/github/gunkim/climbingcalendar/infrastructure/domain/jpa/schedule/dao/ScheduleDao.java
+++ backend/src/main/java/github/gunkim/climbingcalendar/infrastructure/domain/jpa/schedule/dao/ScheduleDao.java
@@ -3,6 +3,7 @@ package github.gunkim.climbingcalendar.infrastructure.domain.jpa.schedule.dao;
 import github.gunkim.climbingcalendar.infrastructure.domain.jpa.schedule.entity.ScheduleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -13,19 +14,19 @@ public interface ScheduleDao extends JpaRepository<ScheduleEntity, Long> {
 
     @Query(
             """
-            SELECT COUNT(s)
-            FROM schedule s
-            WHERE s.userId = :userId AND FUNCTION('YEAR', s.scheduleDate) = :year
-            """
+                    SELECT COUNT(s)
+                    FROM schedule s
+                    WHERE s.userId = :userId AND FUNCTION('YEAR', s.scheduleDate) = :year
+                    """
     )
-    int countByUserIdAndYear(Long userId, int year);
+    int countByUserIdAndYear(@Param("userId") Long userId, @Param("year") int year);
 
     @Query(
             """
-            SELECT COUNT(s)
-            FROM schedule s
-            WHERE s.userId = :userId AND FUNCTION('MONTH', s.scheduleDate) = :month
-            """
+                    SELECT COUNT(s)
+                    FROM schedule s
+                    WHERE s.userId = :userId AND FUNCTION('MONTH', s.scheduleDate) = :month
+                    """
     )
-    int countByUserIdAndMonth(Long userId, int month);
+    int countByUserIdAndMonth(@Param("userId") Long userId, @Param("month") int month);
 }

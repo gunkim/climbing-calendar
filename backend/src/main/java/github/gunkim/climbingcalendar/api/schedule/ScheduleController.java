c71134556344c9825efcec7a192918ec94dbb194
gunkim <gunkim.dev@gmail.com>
2025-04-08 18:38:32 +0900

    클리어 데이터 변환 로직 추가

--- backend/src/main/java/github/gunkim/climbingcalendar/api/schedule/ScheduleController.java
+++ backend/src/main/java/github/gunkim/climbingcalendar/api/schedule/ScheduleController.java
@@ -18,15 +18,7 @@ import github.gunkim.climbingcalendar.domain.schedule.service.GetSchedulesCountS
 import github.gunkim.climbingcalendar.domain.schedule.service.UpdateScheduleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -76,7 +68,7 @@ public class ScheduleController implements ScheduleResource {
     @Override
     public void createSchedule(AuthenticatedUser authenticatedUser, CreateScheduleRequest createScheduleRequest) {
         createScheduleService.createSchedule(authenticatedUser.userId(), ClimbingGymId.from(createScheduleRequest.climbingGymId()),
-                createScheduleRequest.title(), createScheduleRequest.memo(), createScheduleRequest.scheduleDate(), createScheduleRequest.clearList());
+                createScheduleRequest.title(), createScheduleRequest.memo(), createScheduleRequest.scheduleDate(), createScheduleRequest.toClearCommands());
     }
 
     @Override

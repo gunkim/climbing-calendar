e1b5cce43403f39004a8abb9c042f580e075048d
PARKGE <phg5459@naver.com>
2025-04-08 19:08:09 +0900

    GET /api/v1/climbing-gyms/count 매핑
    암장 방문 및 클리어 횟수를 조회할 수 있는 새로운 API를 추가했습니다.

--- backend/src/main/java/github/gunkim/climbingcalendar/api/climbinggym/ClimbingGymController.java
+++ backend/src/main/java/github/gunkim/climbingcalendar/api/climbinggym/ClimbingGymController.java
@@ -1,15 +1,18 @@
 package github.gunkim.climbingcalendar.api.climbinggym;
 
+import github.gunkim.climbingcalendar.api.AuthenticatedUser;
+import github.gunkim.climbingcalendar.api.climbinggym.request.GetClimbingGymOfVisitAndClearCountRequest;
 import github.gunkim.climbingcalendar.api.climbinggym.response.GetClimbingGymOfLevelResponse;
+import github.gunkim.climbingcalendar.api.climbinggym.response.GetClimbingGymOfVisitAndClearCountResponse;
 import github.gunkim.climbingcalendar.api.climbinggym.response.GetClimbingGymResponse;
+import github.gunkim.climbingcalendar.application.ClimbingGymQueryService;
+import github.gunkim.climbingcalendar.application.model.ClimbingGymOfVisitAndClearCountCriteria;
 import github.gunkim.climbingcalendar.domain.climbinggym.model.id.ClimbingGymId;
 import github.gunkim.climbingcalendar.domain.climbinggym.service.GetClimbingGymService;
 import github.gunkim.climbingcalendar.domain.climbinggym.service.GetLevelService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,7 +22,11 @@ interface ClimbingGymResource {
     List<GetClimbingGymResponse> getClimbingGyms();
 
     @GetMapping("/{id}/levels")
-    List<GetClimbingGymOfLevelResponse> getClimbingGymOfLevels(@PathVariable long id);
+    List<GetClimbingGymOfLevelResponse> getClimbingGymOfLevels(@PathVariable("id") long id);
+
+    @GetMapping("/count")
+    List<GetClimbingGymOfVisitAndClearCountResponse> getClimbingGymOfVisitAndClearCounts(@AuthenticationPrincipal AuthenticatedUser authenticatedUser, @ModelAttribute
+    GetClimbingGymOfVisitAndClearCountRequest getClimbingGymOfVisitAndClearCountRequest);
 }
 
 @RestController
@@ -27,6 +34,7 @@ interface ClimbingGymResource {
 public class ClimbingGymController implements ClimbingGymResource {
     private final GetClimbingGymService getClimbingGymService;
     private final GetLevelService getLevelService;
+    private final ClimbingGymQueryService climbingGymQueryService;
 
     public List<GetClimbingGymResponse> getClimbingGyms() {
         return getClimbingGymService.getClimbingGyms().stream()
@@ -37,6 +45,15 @@ public class ClimbingGymController implements ClimbingGymResource {
     public List<GetClimbingGymOfLevelResponse> getClimbingGymOfLevels(long id) {
         return getLevelService.getLevels(ClimbingGymId.from(id)).stream()
                 .map(GetClimbingGymOfLevelResponse::from)
+                .toList();
+    }
+
+    @Override
+    public List<GetClimbingGymOfVisitAndClearCountResponse> getClimbingGymOfVisitAndClearCounts(AuthenticatedUser authenticatedUser,
+                                                                                                GetClimbingGymOfVisitAndClearCountRequest getClimbingGymOfVisitAndClearCountRequest) {
+        return climbingGymQueryService.getClimbingGymOfVisitAndClearCounts(ClimbingGymOfVisitAndClearCountCriteria.of(authenticatedUser.userId(),
+                        getClimbingGymOfVisitAndClearCountRequest.limit(), getClimbingGymOfVisitAndClearCountRequest.orderBy())).stream()
+                .map(GetClimbingGymOfVisitAndClearCountResponse::from)
                 .toList();
     }
 }

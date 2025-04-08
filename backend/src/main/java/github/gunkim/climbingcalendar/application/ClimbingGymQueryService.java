d8eb0d31a96b209b99bd0e2b2e541b1dea4e76fa
PARKGE <phg5459@naver.com>
2025-04-08 19:56:46 +0900

    불필요한 줄바꿈 제거

--- backend/src/main/java/github/gunkim/climbingcalendar/application/ClimbingGymQueryService.java
+++ backend/src/main/java/github/gunkim/climbingcalendar/application/ClimbingGymQueryService.java
@@ -7,5 +7,4 @@ import java.util.List;
 
 public interface ClimbingGymQueryService {
     List<ClimbingGymOfVisitAndClearCountResult> getClimbingGymOfVisitAndClearCounts(ClimbingGymOfVisitAndClearCountCriteria criteria);
-
 }
